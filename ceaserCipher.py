f52f80629101624b592b3624dbe46b6d0a54c51c
ABHIRAM_S <149877367+Abhiram-ARS@users.noreply.github.com>
2025-12-06 20:22:15 +0530

    Refactor Caesar cipher into run function

--- ceaserCipher.py
+++ ceaserCipher.py
@@ -1,25 +1,24 @@
-def caesar_encrypt(text, key):
+def run():
+    def caesar_encrypt(text, key):
 
-    result = ""
+        result = ""
 
-    for char in text:
-        if char.isalpha():
-            start_val = ord('a') if char.islower() else ord('A')           
-            shifted_char_index = (ord(char) - start_val + key) % 26
-            new_char = chr(start_val + shifted_char_index)
+        for char in text:
+            if char.isalpha():
+                start_val = ord('a') if char.islower() else ord('A')           
+                shifted_char_index = (ord(char) - start_val + key) % 26
+                new_char = chr(start_val + shifted_char_index)
+                
+            else:
+                new_char = char
+
+            result += new_char
             
-        else:
-            new_char = char
-
-        result += new_char
-        
-    return result
+        return result
 
 
-while True:
-    text = input("Caesar_Cipher_Bruteforce >")
-    if text == 'exit':
-        break
+
+    text = input("\nCaesar_Cipher_Bruteforce >")
     print("\n")
     shift_key = 3
     for key in range(27):
